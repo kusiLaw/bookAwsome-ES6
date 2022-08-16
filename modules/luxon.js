fcdae35066b8de513113d34a408d61e7af63f7eb
kusi <lawcubegsm@gmail.com>
2022-08-16 08:26:38 +0000

    Fix linter error

--- modules/luxon.js
+++ modules/luxon.js
@@ -1,6 +1,5 @@
 // import { DateTime } from '../node_modules/luxon/src/luxon.js';
-import {DateTime, Duration, Interval} from '../node_modules/luxon/build/es6/luxon.js'
-
+import { DateTime } from '../node_modules/luxon/build/es6/luxon.js';
 
 const mydatetime = () => DateTime.now().toFormat('MMMM dd, yyyy h:m:s');
 
